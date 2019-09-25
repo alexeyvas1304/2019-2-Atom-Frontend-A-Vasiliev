7aac1204da461bf4b8f70a672033f6dc732b2dd7
Alexey <nikson1708@yandex.ru>
2019-09-25 16:54:37 +0300

    first commit

---
+++ build/precache-manifest.b7f8ca4dc1aac2199c05cbdd69f38eb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ec500bf814ee495a9a468f41d70def",
+    "url": "/2019-2-Atom-Frontend-A-Vasiliev/index.html"
+  },
+  {
+    "revision": "5beefb0d326632c0998d",
+    "url": "/2019-2-Atom-Frontend-A-Vasiliev/static/css/main.b4eae362.chunk.css"
+  },
+  {
+    "revision": "f8b18effe52a3f5a6b30",
+    "url": "/2019-2-Atom-Frontend-A-Vasiliev/static/js/2.1556f18d.chunk.js"
+  },
+  {
+    "revision": "5beefb0d326632c0998d",
+    "url": "/2019-2-Atom-Frontend-A-Vasiliev/static/js/main.0e6fdcd1.chunk.js"
+  },
+  {
+    "revision": "4decaeab0fdc270759de",
+    "url": "/2019-2-Atom-Frontend-A-Vasiliev/static/js/runtime-main.1c821476.js"
+  },
+  {
+    "revision": "a55f1c4358068374afaef45a15ab0eb8",
+    "url": "/2019-2-Atom-Frontend-A-Vasiliev/static/media/logo.a55f1c43.svg"
+  }
+]);
